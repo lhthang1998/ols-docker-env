29bed6c576c56a602f946f21a535e32571e6662e
Thang Le <lhthang.1998@gmail.com>
2023-03-06 07:52:50 +0700

    update sftp

--- user_scripts/install-sftp.sh
+++ user_scripts/install-sftp.sh
@@ -9,12 +9,14 @@ if [[ ! -z $(grep "Match User $username" "/etc/ssh/sshd_config") ]];
 then
 echo "Already set up"
 else
+echo "Add new group: sftp_user"
+sudo groupadd sftp_user
 echo "Add user to sftp server"
 echo -e "\n\n\n\n\n\y\n" |  sudo adduser --home $dir_name $username
 echo -e "$passwd\n$passwd\n" | sudo passwd $username
 #sudo chmod 755 $dir_name
-sudo chown $username:$username $dir_name
-sudo chmod 755 $dir_name
+sudo chgrp -R sftp_user $dir_name
+sudo chmod -R 770 $dir_name
 #cat << EOF >> /etc/ssh/sshd_config
 #Match User $username
 #  ChrootDirectory $dir_name
